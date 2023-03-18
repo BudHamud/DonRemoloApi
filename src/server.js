e04248b8e3448e34922b60f46c1078312aeec9cd
BudHamud <nigitart@gmail.com>
2023-03-18 14:20:00 -0300

    agregando cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors'
 import homeRouter from "./routes/home.router.js";
 import "./dao/dbConfig.js";
 import "dotenv/config";
@@ -6,6 +7,7 @@ import "dotenv/config";
 const app = express();
 const PORT = process.env.MONGOPORT;
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
